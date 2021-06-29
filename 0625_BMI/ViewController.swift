afc69fb59a37a93929d5d0fc99f62d7b19628de9
zoeliauh <ibm801227@hotmail.com>
2021-06-29 11:59:36 +0800

    Initial Commit

---
+++ 0625_BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0625_BMI
+//
+//  Created by woanjwu liauh on 2021/6/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
